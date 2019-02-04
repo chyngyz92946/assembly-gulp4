fedc73435af957ce1e2f342b4c90f8a6c73d8bce
chyngyz <chika9294@gmail.com>
2019-02-04 14:51:16 +0600

    bootstrap 4.2.1

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,7 +1,8 @@
 module.exports = function () {
 	$.gulp.task('scripts:lib', function () {
 		return $.gulp.src(['node_modules/jquery/dist/jquery.min.js', 
-			'node_modules/slick-carousel/slick/slick.min.js'])
+			'node_modules/slick-carousel/slick/slick.min.js',
+			'node_modules/bootstrap/dist/js/bootstrap.min.js'])
 			.pipe($.gp.concat('libs.min.js'))
 			.pipe($.gp.uglifyjs())
 			.pipe($.gulp.dest('build/static/js/'))
